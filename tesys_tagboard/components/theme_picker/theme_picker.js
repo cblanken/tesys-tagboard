6139ebf6c370e20987a81731025a63f746168ba1
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-11-11 22:52:49 -0500

    refactor: isolate theme-picker JS to prevent variable conflicts

--- tesys_tagboard/components/theme_picker/theme_picker.js
+++ tesys_tagboard/components/theme_picker/theme_picker.js
@@ -1,10 +1,12 @@
-let theme_picker = document.querySelector("#theme-picker");
+(function () { // Self invoking function to avoid variable clashing
+  let theme_picker = document.querySelector("#theme-picker");
 
-theme_picker.querySelectorAll("li").forEach(li => {
-  li.addEventListener("click", e => {
-    console.log(li);
-    let theme = li.querySelector("input").value;
-    document.querySelector("html").setAttribute("data-theme", theme);
-    window.localStorage.setItem("theme", theme);
+  theme_picker.querySelectorAll("li").forEach(li => {
+    li.addEventListener("click", e => {
+      console.log(li);
+      let theme = li.querySelector("input").value;
+      document.querySelector("html").setAttribute("data-theme", theme);
+      window.localStorage.setItem("theme", theme);
+    });
   });
-});
+})();
