f2c669c797f42146566be1ffa8fd2fe059d4bc28
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-11-26 18:45:20 -0500

    fix: navbar links on mobile
    - Remove global project.js Javascript and isolate clickthrough prevention
    to the post_thumbnail component since the `.dropdown` class query was
    catching all dropdowns including the own used in the navbar on
    mobile-sized screens
    - Simplify text size styles by using the appropriate menu-xl DaisyUI class

--- tesys_tagboard/components/post_thumbnail/post_thumbnail.js
+++ tesys_tagboard/components/post_thumbnail/post_thumbnail.js
@@ -1,5 +1,4 @@
 (function () { // Self invoking function to avoid variable clashing
-
   function show_thumbnail(container, img) {
     img.classList.add(`animate-pop-in-fast`);
     img.classList.add(`motion-reduce:animate-fade-in-fast`);
@@ -9,6 +8,12 @@
 
   document.querySelectorAll(`.post-thumbnail`).forEach((thumbnail) => {
     const img = thumbnail.querySelector(`img`);
+
+    thumbnail.querySelectorAll(".dropdown").forEach(ele => {
+      ele.addEventListener("click", (e) => {
+        e.preventDefault();
+      })
+    });
 
     if (img.complete) {
       show_thumbnail(thumbnail, img);
--- tesys_tagboard/static/js/project.js
+++ tesys_tagboard/static/js/project.js
@@ -1,8 +1,1 @@
 /* Project specific Javascript goes here. */
-document.querySelectorAll('.dropdown, button.btn:not([type="submit"])').forEach(btn => {
-  btn.addEventListener("click", (e) => {
-    // Stop regular buttons from clicking "through" elements
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
