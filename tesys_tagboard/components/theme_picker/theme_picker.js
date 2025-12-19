8e77fc6ae5bee26c67c06d2d0aed950bdd4e4589
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-12-19 17:11:21 -0500

    fix: highlight selected theme in theme picker

--- tesys_tagboard/components/theme_picker/theme_picker.js
+++ tesys_tagboard/components/theme_picker/theme_picker.js
@@ -1,10 +1,29 @@
 (function () { // Self invoking function to avoid variable clashing
   let theme_picker = document.querySelector("#theme-picker");
+
+  const highlight_current_theme = () => {
+    const active_theme = window.localStorage.getItem("theme");
+
+    if (active_theme) {
+      theme_picker.querySelectorAll("li").forEach(li => {
+        const highlight_classes = ["bg-primary", "text-primary-content", "underline"];
+        const input = li.querySelector("input[type='radio']");
+        if (active_theme === input.value) {
+          input.classList.add(...highlight_classes);
+        } else {
+          input.classList.remove(...highlight_classes);
+        }
+      });
+    }
+  }
+
+  highlight_current_theme();
   theme_picker.querySelectorAll("li").forEach(li => {
     li.addEventListener("click", e => {
       let theme = li.querySelector("input").value;
       document.querySelector("html").setAttribute("data-theme", theme);
       window.localStorage.setItem("theme", theme);
+      highlight_current_theme();
     });
   });
 })();
