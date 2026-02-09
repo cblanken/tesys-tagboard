30544d1a4112c33673996caf3c359081fc542e96
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2026-02-09 15:12:34 -0500

    feat: dynamic autocomplete by cursor position
    Setup the autocomplete parsing to detect the closest, previous token
    in a query and perform autocompletion by using that as a partial arg.
    This way the user can modify an already written token in the middle of
    a query and still get autocompletion feedback.
    
    Sort autocomplete filters to the top by default for better
    discoverability. There shouldn't be too many filters that it ever
    really becomes an issue of too many filters pushing down the tag
    autocompletions.
    
    When no partial argument is passed, the default is to use the last
    token in the query.

--- tesys_tagboard/components/search_bar/search_bar.js
+++ tesys_tagboard/components/search_bar/search_bar.js
@@ -1,6 +1,7 @@
 (function () { // Self invoking function to avoid variable clashing
   const setup_search_bar = (root) => {
     const search_input = root.querySelector("input[type='search']");
+    const partial_input = root.querySelector("input[name='partial']");
     const result_container = root.querySelector(".result-container");
     let autocomplete_items = [];
     let active_autocomplete_index = -1;
@@ -25,8 +26,6 @@
         try {
           htmx.remove(search_results_ul)
         } catch (NotFoundError) {
-          console.log(root)
-          console.log(result_container)
           console.error(`Couldn't find search results ul`)
         }
       }
@@ -74,6 +73,13 @@
       reset_active_autocomplete_index();
     });
 
+    search_input.addEventListener("input", (e) => {
+      let partial = get_query_active_token_selection().partial;
+      partial_input.value = partial;
+      let event = new Event("search_input_changed")
+      dispatchEvent(event);
+    });
+
     htmx.on(search_input, "change", (e) => {
       result_container.querySelectorAll("li").forEach(li => {
         htmx.on(li, "focus", (e) => {
@@ -87,6 +93,80 @@
         });
       });
     });
+
+    const split_re = /\s+/i;
+    function get_query_active_token_selection() {
+      // Identifies the token actively being edited and parses it to provide a partial selection
+      // for the autocomplete. For example if a user is writing a query: `abc 123^ def 456` where
+      // the cursor is at the position of "^". The autocomplete will be based on the the nearest
+      // previous token, so 123 in this example which will be provided as the partial argument to
+      // the autocomplete endpoint.
+      let query = search_input.value;
+
+      // Find end of token index or default to end of query
+      i = search_input.selectionStart
+      let token_end = query.indexOf(" ", i)
+      if (token_end === -1) {
+        token_end = query.length
+      }
+
+      // Find start of token index
+      let token_start = 0;
+      let token_start_index_offset = query.slice(0, i).split("").reverse().indexOf(" ")
+      if (token_start_index_offset !== -1) {
+        token_start = i - token_start_index_offset;
+      }
+
+      const partial = query.slice(token_start, token_end)
+      return { start: token_start, end: token_end, partial: partial };
+    }
+
+    const add_autocomplete_item_to_input = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      let active_token = get_query_active_token_selection();
+      let query = String(search_input.value)
+
+      const autocompleted_name = e.currentTarget?.dataset.name
+      if (autocompleted_name == undefined) {
+        console.error("Search autocomplete failed to parse the provided query.");
+        return
+      }
+
+      let new_query = `${query.slice(0, active_token.start)}${autocompleted_name}${query.slice(active_token.end)}`;
+      search_input.value = new_query;
+
+      search_input.focus();
+    }
+
+    htmx.on(root.querySelector(".result-container"), "htmx:afterSettle", (e) => {
+      let search_results = get_search_results();
+      if (search_results) {
+        Array.from(search_results.children).forEach(autocomplete_item => {
+          htmx.on(autocomplete_item, "click", add_autocomplete_item_to_input);
+          htmx.on(autocomplete_item, "keydown", (e) => {
+            e.preventDefault();
+            switch (e.code) {
+              case "Enter":
+                add_autocomplete_item_to_input(e)
+                break;
+              case "Escape":
+                search_input.focus();
+                break;
+              case "ArrowLeft":
+                search_input.focus();
+                break;
+              case "ArrowRight":
+                search_input.focus();
+                break;
+              default:
+                // Do nothing
+            }
+          });
+        });
+      }
+    });
+
   }
 
   const setup = (search_bars) => {
