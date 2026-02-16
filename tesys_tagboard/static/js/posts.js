153e013337d79690d327fd5839d8d2b301d846a2
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2026-02-16 17:28:39 -0500

    feat: add advanced and simple search mode toggle

---
+++ tesys_tagboard/static/js/posts.js
@@ -0,0 +1,38 @@
+const simple_search_form = document.querySelector("form#simple-post-search");
+const advanced_search_form = document.querySelector("form#advanced-post-search");
+const edit_advanced_search_btn = document.querySelector("button#edit-advanced-search-btn");
+const edit_advanced_search_btn_text = document.querySelector("#edit-advanced-search-btn-text");
+const search_submit_btn = document.querySelector("#search-submit-btn");
+
+let advanced_search_hidden = true;
+const toggle_advanced_search_off_form_classes = ["hidden"];
+const toggle_simple_search_off_form_classes = ["hidden"];
+const toggle_advanced_search_off_btn_classes = [];
+const toggle_advanced_search = () => {
+  if (advanced_search_hidden) {
+    // Enable advanced search mode
+    simple_search_form.classList.add(...toggle_simple_search_off_form_classes);
+    advanced_search_form.classList.remove(...toggle_advanced_search_off_form_classes);
+    edit_advanced_search_btn.classList.add(...toggle_advanced_search_off_btn_classes);
+    edit_advanced_search_btn_text.textContent = "SIMPLE"
+    search_submit_btn.setAttribute("form", "advanced-post-search");
+  } else {
+    // Enable simple search mode (default)
+    simple_search_form.classList.remove(...toggle_simple_search_off_form_classes);
+    advanced_search_form.classList.add(...toggle_advanced_search_off_form_classes);
+    edit_advanced_search_btn.classList.remove(...toggle_advanced_search_off_btn_classes)
+    edit_advanced_search_btn_text.textContent = "ADVANCED";
+    search_submit_btn.setAttribute("form", "simple-post-search");
+  }
+
+  advanced_search_hidden = !advanced_search_hidden;
+}
+
+edit_advanced_search_btn.addEventListener("click", e => {
+  e.preventDefault();
+  toggle_advanced_search();
+});
+
+
+search_submit_btn.addEventListener("click", e => {
+})
