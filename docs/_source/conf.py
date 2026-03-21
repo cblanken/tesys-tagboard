18cb0234bd9890d124e6f627c51b7744e28224da
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2026-03-21 17:37:05 -0400

    chore(conf): move sphinx conf.py to _sources
    Sphinx expects the conf.py to be in the SOURCE_DIR by default.

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -1,4 +1,4 @@
-# ruff: noqa: ERA001, PTH100
+# ruff: noqa: ERA001, PTH100  # noqa: INP001
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
