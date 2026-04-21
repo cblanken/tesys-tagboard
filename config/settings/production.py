583aa92a6bef3bfe99556426102d193c6f044d4d
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2026-04-21 11:48:09 -0400

    fix: prod database connection exhaustion
    The production deployment runs the application in ASGI mode. See the
    start script at compose/production/django/start.
    
    Django apps run in ASGI mode should not enable the CONN_MAX_AGE
    database setting per the docs:
    https://docs.djangoproject.com/en/6.0/ref/databases/#persistent-connections
    
    Also update the production start script to use the native ASGI worker
    for Gunicorn instead of the UvicornWorker. See the docs for details:
    https://gunicorn.org/asgi/#asgi-worker
    
    NOTE: this should only be a stopgap fix until a proper connection
     pooling solution can be configured. See issue #146

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,4 @@
 from .base import *  # noqa: F403
-from .base import DATABASES
 from .base import INSTALLED_APPS
 from .base import REDIS_URL
 from .base import env
@@ -11,10 +10,6 @@ PRODUCTION = True
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
-
-# DATABASES
-# ------------------------------------------------------------------------------
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 
 # CACHES
 # ------------------------------------------------------------------------------
